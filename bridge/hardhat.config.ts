b1baaa3706d8860f90220122850949ae19426ee1
Nobuhito Kurose <pudding.heart.hd@gmail.com>
2022-09-17 14:53:51 +0900

    add bridge scripts

--- bridge/hardhat.config.ts
+++ bridge/hardhat.config.ts
@@ -4,12 +4,13 @@ import "@typechain/hardhat";
 import "@openzeppelin/hardhat-upgrades";
 import "@nomiclabs/hardhat-etherscan";
 import "@nomiclabs/hardhat-waffle";
+import "hardhat-gas-reporter";
 import "dotenv/config";
 import { HardhatUserConfig } from "hardhat/config";
 
 const accounts = process.env.DEPLOYER ? [`${process.env.DEPLOYER}`] : [];
 
-const config: HardhatUserConfig = {
+export const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
@@ -31,7 +32,6 @@ const config: HardhatUserConfig = {
       chainId: 137,
       url: process.env.RPC_POLYGON || "https://polygon-rpc.com/",
       accounts,
-      // gasPrice: 50e9,
     },
     bnb: {
       chainId: 56,
@@ -41,6 +41,27 @@ const config: HardhatUserConfig = {
     avalanche: {
       chainId: 43114,
       url: process.env.RPC_AVALANCHE || "https://api.avax.network/ext/bc/C/rpc",
+      accounts,
+    },
+    bnbTestnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      chainId: 97,
+      accounts,
+    },
+    mumbai: {
+      url:
+        process.env.MUMBAI_RPC_ENDPOINT ||
+        "https://matic-mumbai.chainstacklabs.com",
+      accounts,
+      gasPrice: 15,
+    },
+    avalancheTestnet: {
+      url: " https://api.avax-test.network/ext/bc/C/rpc",
+      accounts,
+    },
+    arbitrum: {
+      url: "https://arb1.arbitrum.io/rpc",
+      chainId: 42161,
       accounts,
     },
   },
