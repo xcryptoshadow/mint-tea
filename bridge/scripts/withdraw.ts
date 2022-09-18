b1f048547597303b6054ae7510d4087d58c1fdf6
Nobuhito Kurose <pudding.heart.hd@gmail.com>
2022-09-18 18:03:24 +0900

    withdraw locked NFT.

--- bridge/scripts/withdraw.ts
+++ bridge/scripts/withdraw.ts
@@ -1,32 +1,39 @@
+import { DEFAULT_DEBRIDGE_GATE_ADDRESS } from "@debridge-finance/desdk/lib/evm/context";
 import { BigNumber } from "ethers";
 import { config, ethers } from "hardhat";
 
 const NFT_BRIDGE_ADDRESS = "0x320Af97E6E8C580D6850890C81fd7161a3332C71";
-const TARGET_NFT_ADDRESS = "0x03e055692e77e56aBf7f5570D9c64C194BA15616";
+const TARGET_NFT_ADDRESS = "0x3efDF0B78c2CacC94D8EeF01981668521562175b";
 const TOKEN_ID = 5001;
-const GATE_PROTOCOL_FEE = BigNumber.from("500000000000000000"); // 0.5 matic
 const REFERRAL_CODE = 0;
 
-// Polygon to Avalanche
-// const CHAIN_ID_TO = config.networks.avalanche.chainId!;
-// Polygon to Arbitrum
-const CHAIN_ID_TO = config.networks.arbitrum.chainId!;
+const CHAIN_ID_TO = config.networks.polygon.chainId!;
 
 async function main() {
   const [owner] = await ethers.getSigners();
+
+  const deBridgeGate = new ethers.Contract(
+    DEFAULT_DEBRIDGE_GATE_ADDRESS,
+    [
+      "function claim(bytes32 _debridgeId,uint256 _amount,uint256 _chainIdFrom,address _receiver,uint256 _nonce,bytes calldata _signatures,bytes calldata _autoParams) external",
+      "function globalFixedNativeFee() view returns (uint256)",
+    ],
+    owner
+  );
+
+  const gateProtocolFee: BigNumber = await deBridgeGate.globalFixedNativeFee();
+  console.log("ProtocolFee: ", gateProtocolFee);
+
   const nftBridge = await ethers.getContractAt("NFTBridge", NFT_BRIDGE_ADDRESS);
 
-  const targetNFT = new ethers.Contract(
+  const targetNFT = await new ethers.Contract(
     TARGET_NFT_ADDRESS,
     [
-      "function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256)",
       "function approve(address to, uint256 tokenId) public",
       "function tokenURI(uint256 tokenId) public view returns (string memory)",
     ],
     owner
   );
-
-  // const token_id = await targetNFT.tokenOfOwnerByIndex(owner.address, 0);
 
   const token_id = TOKEN_ID;
   const approveTx = await targetNFT.approve(NFT_BRIDGE_ADDRESS, token_id);
@@ -40,7 +47,7 @@ async function main() {
     0,
     REFERRAL_CODE,
     {
-      value: GATE_PROTOCOL_FEE,
+      value: gateProtocolFee,
     }
   );
 
